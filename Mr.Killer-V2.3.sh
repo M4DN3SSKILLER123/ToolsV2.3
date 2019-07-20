e506c5dc95af1c641da81b9ec5f8d2616b55566e
M4DN3SSKILLER123 <52645408+M4DN3SSKILLER123@users.noreply.github.com>
2019-07-20 22:12:08 +0700

    Add files via upload

---
+++ Mr.Killer-V2.3.sh
@@ -0,0 +1,553 @@
+##########################################
+#Tools Mr.Killer Versi :2.1              |
+#Tools Berisi script yang tidak berbahaya|
+#Author : Mr.Killer dan Thunder killer   |
+##########################################
+
+
+echo "hai nama saya Mr.Killer Di update kali ini"
+sleep 1
+echo "Banyak Fitur nya"
+sleep 1
+echo "Tunggu lagi update terbaru dari saya ya"
+sleep 1
+echo "Siapa Nama Anda"
+sleep 1
+read -p "[Nick Anda]" Nick
+echo "hai $Nick Selamat datang di updatetan terbaru kami"
+sleep 1
+echo ""
+echo "\033[33;1m=======================================[>"
+echo "\033[32;1mAuthor : Mr.Killer & Thunder killer"
+echo "\033[35;1mTeam : Cyber Attacker Cirebon[!]"
+echo "\033[33;1mKontak Me :  083120840944"
+echo "\033[31;1mSubcribe Youtube:M4DN3SS GAMING"
+echo "\033[33;1mUpload to git : None" "\033[32;1m&" 
+echo "\033[34;1m[======================================[>"
+sleep 1
+echo ""
+echo $green "Selamat Datang Pengguna"
+echo $red "Di Tools versi ini telah banyak perubahan"
+echo ""
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:"
+echo "[===============================================[>"
+echo $green "1.> Spam Sms Tokopedia"
+echo "[===============================================]>"
+echo $purple "2.> Spam Sms Telkomnyet"
+echo "[===============================================]>"
+echo $cyan "3.> Spam Sms Matahari Mall"
+echo "[===============================================]>"
+echo $red "4.> Spam Sms HooqTV"
+echo "[===============================================]>"
+echo $white "5.> Spam Sms KFC"
+echo "[===============================================]>"
+echo $green "6.> Spam Sms PHD"
+echo "[===============================================]>"
+echo $purple "7.> Spam Sms Whiskas"
+echo "[===============================================]>"
+echo $cyan "8.> Spam Sms ZiPAY"
+echo "[===============================================]>"
+echo $red "9.> Nyari CC buat Carding"
+echo "[===============================================]>"
+echo $white "10.> Spam Akun Gmail"
+echo "[===============================================]>"
+echo $green "11.> Whois LookUp"
+echo "[===============================================]>"
+echo $purple "12.> Upload Admin Finder"
+echo "[===============================================]>"
+echo $cyan "13.> Script Deface Creator"
+echo "[===============================================]>"
+echo $red "14.> ReConDog Buat Information gathering"
+echo "[===============================================]>"
+echo $white "15.> Admin Login Finder"
+echo "[===============================================]>"
+echo $green "16.> Virus VbugMaker Creator"
+echo "[===============================================]>"
+echo $purple "17.> dowload script deface orang"
+echo "[===============================================]>"
+echo $cyan "18.> Autolike Facebook"
+echo "[===============================================]>"
+echo $red "19.> Facebook Auto_Reaction"
+echo "[===============================================]>"
+echo $white "20.> Bot Komentar Facebook"
+echo "[===============================================]>"
+echo $green "21.> Install txtool"
+echo "[===============================================]>"
+echo $purple "22.> install sqlscan"
+echo "[===============================================]>"
+echo $cyan "23.> install hash-buster"
+echo "[===============================================]>"
+echo $red "24.> Install Red_Hawk"
+echo "[===============================================]>"
+echo $white "25.> Install Torshammer buat DDOS website"
+echo "[===============================================]>"
+echo $green "26.> Install LITEDDOS buat DDOS website"
+echo "[===============================================]>"
+echo $purple "27.> Install Hunner Framework"
+echo "[===============================================]>"
+echo $cyan "28.> Install Sqlmap Buat Inject Situs"
+echo "[===============================================]>"
+echo $red "29.> Install 4wsectools"
+echo "[===============================================]>"
+echo $white "30.> Install Lazymux Buat Install Tools"
+echo "[===============================================]>"
+echo $green "31.> Mempercepat / Menstabilkan Jaringan/sinyal"
+echo "[===============================================]>"
+echo $purple "32.> Mempercepat / menstabilkan jaringan 2"
+echo "[===============================================]>"
+echo $cyan "33.> Install BinGoo"
+echo "[===============================================]>"
+echo $red "34.> Full Bot Facebook"
+echo "[===============================================]>"
+echo $white "35.> Install Termux-A"
+echo "[===============================================]>"
+echo $green "36.> Install Tools GadoGado"
+echo "[===============================================]>"
+echo $purple "37.> Install Lazysqlmap"
+echo "[===============================================]>"
+echo $cyan "38.> Install tools diejoubu"
+echo "[===============================================]>"
+echo $red "39.> Install weeman Buat Phising"
+echo "[===============================================]>"
+echo $white "40.> Install iesDEFACE buat Deface Poc Webdav"
+echo "[===============================================]>"
+echo $cya  "41.> Bosan dengan tools ini Install Lainnya"
+echo "[===============================================]>"
+echo $yellow "0.> keluar"
+echo "[===============================================]>"
+echo "\033[32;1m"
+
+read -p "[PILIH AJA KAWAN:~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "Mr.Killer" | lolcat
+cd module
+php tp.php
+fi
+
+if
+[ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet "Mr.Killer"
+cd module
+php t.php
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+figlet "Mr.Killer"
+cd module
+php mataharimall.php
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+toilet "Mr.Killer"
+cd module
+php hooq.php
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+toilet -f slant --gay "Mr.Killer"
+cd module
+php kfc.php
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+toilet -f mono12 -F gay "Mr.Killer"
+cd module
+php phd.php
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+toilet "Mr.Killer"
+cd module
+php whiskas.php
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+figlet "Mr.Killer"
+cd module
+php zipay.php
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+toilet "Mr.Killer"
+cd module
+php key.php
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+toilet "Mr.Killer"
+cd module
+python2 SpamMail.py
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+toilet "Mr.Killer"
+cd module
+python2 whs.py
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+toilet "Mr.Killer"
+cd module
+python2 upf.py
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+toilet "Mr.Killer"
+cd module
+python2 create.py
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+toilet "Mr.Killer"
+cd module
+python2 dog.py
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+toilet "Mr.Killer"
+cd module
+python2 gam.py
+fi
+
+if [ $bro = 16 ] || [ $bro = 16 ]
+then
+clear
+toilet "Mr.Killer"
+cd module
+python2 anvima.py
+fi
+
+if [ $bro = 17 ] || [ $bro = 17 ]
+then
+clear
+toilet "Mr.Killer"
+cd module
+python2 htmd.py
+fi
+
+if [ $bro = 18 ] || [ $bro = 18 ]
+then
+clear
+toilet "Mr.Killer"
+cd module
+php autolike.php
+fi
+
+if [ $bro = 19 ] || [ $bro = 19 ]
+then
+clear
+toilet "Mr.Killer"
+cd module
+python2 auto_reaction.py
+fi
+
+if [ $bro = 20 ] || [ bro = 20 ]
+then
+clear
+figlet "Mr.Killer" | lolcat
+cd module
+python2 botkomen.py
+fi
+
+if [ $bro = 21 ] || [ $bro = 21 ]
+then
+clear
+figlet "Mr.Killer" | lolcat
+git clone https://github.com/kuburan/txtool
+mv txtool $HOME
+cd $HOME/txtool
+python2 install.py
+python2 update.py
+txtool
+fi
+
+
+if [ $bro = 22 ] || [ bro = 22 ]
+then
+clear
+toilet -f slant -F gay "Mr.Killer" 
+pkg install update && pkg install upgrade
+pkg install git
+pkg install php
+cd module
+php sqlscan.php
+fi
+
+if [ $bro = 23 ] || [ $bro = 23 ]
+then
+clear
+toilet -f mono12 -F gay "Mr.Killer" 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/UltimateHackers/Hash-Buster
+mv Hash-Buster $HOME
+cd $HOME/Hash-Buster
+python2 hash.py
+fi
+
+if [ $bro = 24 ] || [ $bro = 24 ]
+then
+clear
+toilet -f standard -F gay "Mr.Killer" 
+echo "\033[33;1muntuk tutorial kunjungi:" "\033[36;1m https://youtu.be/h2I5HU3W-so"
+sleep 3
+apt update
+apt install git
+apt install php
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+mv RED_HAWK $HOME
+cd $HOME/RED_HAWK
+chmod +x rhawk.php
+php rhawk.php
+fi
+
+if [ $bro = 25 ] || [ $bro = 25 ]
+then
+clear
+toilet -f standard -F gay "Mr.Killer" 
+apt update
+apt install git
+apt install tor
+git clone https://github.com/dotfighter/torshammer.git
+mv torshammer $HOME
+cd $HOME/torshammer
+read -p "Masukan Target==>:" target
+python2 torshammer.py -T -t target
+fi
+
+if [ $bro = 26 ] || [ $bro = 26 ]
+then
+clear
+toilet -f standard -F gay "Mr.Killer" 
+echo "\033[33;1muntuk tutorial kunjungi:" "\033[36;1m https://youtu.be/EskgepQSB7U"
+sleep 3
+apt update
+apt install git
+git clone https://github.com/4L13199/LITEDDOS
+mv LITEDDOS $HOME
+cd $HOME/LITEDDOS
+read -p "MasukanTarget:" target
+python2 LITEDDOS.py target 80 100
+fi
+
+if [ $bro = 27  || [ $bro = 27 ]
+then
+clear
+toilet -f standard -F gay "Mr.Killer" 
+apt update
+apt install git
+git clone https://github.com/b3-v3r/Hunner.git
+mv Hunner $HOME
+cd $HOME/Hunner
+chmod +x hunner.py
+python2 hunner.py
+fi
+
+if [ $bro = 28 ] || [ $bro = 28 ]
+then
+clear
+toilet -f standard -F gay "Mr.Killer"
+apt update
+apt install git
+git clone https://github.com/sqlmapproject/sqlmap
+mv sqlmap $HOME
+cd $HOME/sqlmap
+read -p "masukan web target:" target
+python2 sqlmap.py -D target
+fi
+
+if [ $bro = 29 ] || [ $bro = 29 ]
+then
+clear
+toilet -f standard -F gay "Mr.Killer" 
+apt update
+apt install git
+git clone https://github.com/aryanrtm/4wsectools
+mv 4wsectools $HOME
+cd $HOME/4wsectools
+chmod 777 tools
+./tools
+fi
+
+if [ $bro = 30 ] || [ $bro = 30 ]
+then
+clear
+figlet "Mr.Killer" | lolcat
+apt update && upgrade
+apt install python2 
+apt install git
+cd module
+python2 lazymux.py
+fi
+
+if [ $bro = 31 ] || [ $bro = 31 ]
+then
+clear
+figlet "Mr.Killer" | lolcat
+sleep 1
+echo "\033[32;m====================Mempercepat / menstabilkan sinyal===================="
+sleep 1
+echo "\033[32;1m================================By Mr.B4J1N64N================================"
+sleep 1
+echo "Sedang Memuat.."
+echo ""
+ping 8.8.8.8 
+fi
+
+if [ $bro = 32 ] || [ $bro = 32 ]
+then
+clear
+figlet "Mr.Killer" | lolcat
+sleep 1
+ping -D www.google.com
+fi
+
+if [ $bro = 33 ] || [ $bro = 33 ]
+then
+clear
+figlet "Mr.Killer" | lolcat
+sleep 1
+git clone https://github.com/Hood3dRob1n/BinGoo
+mv BinGoo $HOME
+cd $HOME/BinGoo
+bash bingoo
+fi
+
+if [ $bro = 34 ] || [ $bro = 34 ]
+then
+clear
+figlet "Mr.Killer" | lolcat
+sleep 1
+cd module
+python2 facebook.py
+fi
+
+if [ $bro = 35 ] || [ $bro = 35 ]
+then
+clear
+figlet "Mr.Killer" | lolcat
+sleep 1
+apt update && apt upgrade
+apt install termux-api
+apt install git
+apt install php
+git clone https://github.com/Cvar1984/Termux-A.git
+mv Termux-A $HOME
+cd $HOME/Termux-A
+php run.php
+fi
+
+if [ $bro = 36 ] || [ $bro = 36 ]
+then
+clear
+figlet "Mr.Killer" | lolcat
+sleep 1
+apt update && apt upgrade
+apt install ruby
+apt install lolcat
+apt install git
+git clone https://github.com/Senitopeng/GadoGado.git
+mv GadoGado $HOME
+cd $HOME/GadoGado
+bash gado
+fi
+
+if [ $bro = 37 ] || [ $bro = 37 ]
+then
+clear
+figlet "Mr.Killer" | lolcat
+sleep 1
+apt update && apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/verluchie/termux-lazysqlmap.git
+mv termux-lazysqlmap $HOME
+cd $HOME/termux-lazysqlmap
+chmod 777 install.sh
+./install.sh
+lazysqlmap
+fi
+
+if [ $bro = 38 ] || [ $bro = 38 ]
+then
+clear
+figlet "Mr.Killer" | lolcat
+sleep 1 
+apt install git
+apt install php
+git clone https://github.com/alintamvanz/diejoubu
+mv diejoubu $HOME
+cd $HOME/diejoubu
+cd v1.2
+php diejoubu.php
+fi
+
+if [ $bro = 39 ] || [ $bro = 39 ]
+then
+clear
+figlet "Mr.Killer" | lolcat
+sleep 1 
+apt install git
+apt install php
+git clone https://github.com/evait-security/weeman.git
+mv weeman $HOME
+cd $HOME/weeman
+python2 weeman.py
+fi
+
+if [ $bro = 40 ] || [ $bro = 40 ]
+then
+clear
+figlet "Mr.Killer" | lolcat
+sleep 1 
+apt install git
+apt install bash
+pip2 install bash
+git clone https://github.com/ALX-04/iesDEFACE
+mv iesDEFACE $HOME
+cd $HOME/iesDEFACE
+bash iesDeface.sh
+fi
+
+if [ $bro = 0 ] || [ $bro =0 ]
+then
+echo $red "Author : Mr.Killer"
+echo $yelow "Support :Thunder killer"
+echo $blue "Team : Cyber attacker Cirebon"
+echo $green "Termakasih Kepada Seluruh anggota Cyber Attacker Cirebon"
+echo $red "Pesan Penutup jika kalian hacker"
+echo $red "Jadilah Hacker baik oke !!"
+echo $blue "please wait"
+sleep 5
+fi
